6a3662015408993151da5cd56bde0038198a5b92
papertoy1127 <richard1909@naver.com>
2021-10-04 01:09:27 +0900

    no blue color

--- Assets/Scripts/Manager/SceneIntro.cs
+++ Assets/Scripts/Manager/SceneIntro.cs
@@ -6,7 +6,7 @@ using Random = UnityEngine.Random;
 public class SceneIntro : MonoBehaviour{
     private void Update() {
         if (Input.anyKeyDown) {
-            var transition = (Trans) (((long) Random.Range(1, 0b10000)) + ((long) Random.Range(0b100000, 0b1000000000)));
+            var transition = (Trans) Mathf.RoundToInt(Random.Range(0b1, 0b10000)) + (Mathf.RoundToInt(Random.Range(0b100000, 0b1000000000)));
             // transition = Trans.FromRight | Trans.ToLeft 등으로 뱡향 설정
             GameManager.Instance.Transition(transition, () => Debug.Log("asdf"));
         }
