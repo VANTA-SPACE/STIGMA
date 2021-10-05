5950a1cdf2060e029f0b59b9b4bf034d384c1bf8
papertoy1127 <richard1909@naver.com>
2021-10-05 18:22:00 +0900

    기본적인 번역

--- Assets/Scripts/Locale/Translate.cs
+++ Assets/Scripts/Locale/Translate.cs
@@ -2,8 +2,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace Locale {
-    public class Translate {
+    public class Translate : MonoBehaviour {
         public static SystemLanguage CurrentLanguage;
-        public static Dictionary<SystemLanguage, Dictionary<string, string>> translationsDict;
+        public static Dictionary<string, Dictionary<SystemLanguage, string>> TranslationsDict;
+
+        public static bool TryGet(string key, out string value, SystemLanguage? language = null) {
+            language ??= CurrentLanguage;
+            if (!TranslationsDict.ContainsKey(key)) {
+                value = null;
+                return false;
+            }
+
+            var dict2 = TranslationsDict[key];
+
+            if (dict2.TryGetValue(language.Value, out value)) return true;
+            return dict2.TryGetValue(SystemLanguage.English, out value);
+        }
+
+        public static string Get(string key, SystemLanguage? language = null) {
+            if (TryGet(key, out var value, language)) {
+                return value;
+            }
+
+            return $"KeyNotFound {key}";
+        }
     }
 }
