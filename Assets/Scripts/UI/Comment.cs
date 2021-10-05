71a7b933eabb52a008661e6f91216ebe1d08a72d
papertoy1127 <richard1909@naver.com>
2021-10-06 02:21:45 +0900

    주석

---
+++ Assets/Scripts/UI/Comment.cs
@@ -0,0 +1,10 @@
+using System;
+using UnityEngine;
+
+namespace UI {
+    public class Comment : MonoBehaviour {
+        private void Awake() {
+            Destroy(gameObject);
+        }
+    }
+}
