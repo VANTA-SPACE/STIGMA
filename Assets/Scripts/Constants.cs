748736d5e5a0ad0f0a4d4fba242a8addff774720
papertoy1127 <richard1909@naver.com>
2021-10-17 12:07:28 +0900

    Readonlydictionary

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -1,5 +1,7 @@
 // ReSharper disable InconsistentNaming
 
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using Core;
 using UnityEngine;
@@ -36,17 +38,18 @@ public static class Constants {
     
     //노트 속도
     public const float NOTE_SPEED_MODIFIER = 2f;
-    
 
-    public static readonly ConstDict<Judgment, Color> JudgmentColors = new ConstDict<Judgment, Color>(
-        (Judgment.PerfectEarly, new Color(1f, 1f, 0.6f)),
-        (Judgment.PerfectLate, new Color(1f, 1f, 0.6f)),
-        (Judgment.Good, new Color(0.4f, 0.8f, 1f)),
-        (Judgment.GoodEarly, new Color(0.5f, 0.6f, 0.7f)),
-        (Judgment.GoodLate, new Color(0.5f, 0.6f, 0.7f)),
-        (Judgment.Bad, new Color(0.8f, 0.2f, 0.1f)),
-        (Judgment.Miss, new Color(0.6f, 0.6f, 0.6f))
-    );
+
+    public static readonly ReadOnlyDictionary<Judgment, Color> JudgmentColors = new ReadOnlyDictionary<Judgment, Color>(
+        new Dictionary<Judgment, Color> {
+            {Judgment.PerfectEarly, new Color(1f, 1f, 0.6f)},
+            {Judgment.PerfectLate, new Color(1f, 1f, 0.6f)},
+            {Judgment.Good, new Color(0.4f, 0.8f, 1f)},
+            {Judgment.GoodEarly, new Color(0.5f, 0.6f, 0.7f)},
+            {Judgment.GoodLate, new Color(0.5f, 0.6f, 0.7f)},
+            {Judgment.Bad, new Color(0.8f, 0.2f, 0.1f)},
+            {Judgment.Miss, new Color(0.6f, 0.6f, 0.6f)},
+        });
 
     
     public static string DataPath => Application.persistentDataPath;
